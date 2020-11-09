1527c370bfb0b7d03bbc2ff583836419be79a403
ythosa <vasus714@yandex.ru>
2020-11-09 16:40:21 +0300

    Fix connection between microservices

--- api-gateway/src/subscription/subscription.module.ts
+++ api-gateway/src/subscription/subscription.module.ts
@@ -3,23 +3,27 @@ import { SubscriptionService } from './subscription.service';
 import { SubscriptionController } from './subscription.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { join, resolve } from 'path';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
     providers: [SubscriptionService],
     controllers: [SubscriptionController],
     imports: [
-        ClientsModule.register([
+        ClientsModule.registerAsync([
             {
                 name: 'USER_PACKAGE',
-                transport: Transport.GRPC,
-                options: {
-                    package: 'user',
-                    protoPath: join(
-                        resolve(process.cwd(), '..'),
-                        'protobufs/user-service/user.proto',
-                    ),
-                    url: process.env.USER_SERVICE_URL,
-                },
+                useFactory: (configService: ConfigService) => ({
+                    transport: Transport.GRPC,
+                    options: {
+                        package: 'user',
+                        protoPath: join(
+                            resolve(process.cwd(), '..'),
+                            'protobufs/user-service/user.proto',
+                        ),
+                        url: configService.get<string>("USER_SERVICE_URL"),
+                    },
+                }),
+                inject: [ConfigService]
             },
         ]),
     ],
--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -5,6 +5,10 @@ import { typeOrmConfig } from './config/typeorm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    imports: [TypeOrmModule.forRoot(typeOrmConfig), UserModule, configModule],
+    imports: [
+        TypeOrmModule.forRoot(typeOrmConfig),
+        UserModule,
+        configModule
+    ],
 })
 export class AppModule {}
