274b0c4724b6c1ef53e64c19983b86814b8b8ce2
ythosa <vasus714@yandex.ru>
2020-11-11 17:53:52 +0300

    Update docker-compose file & created base items of auth service

--- auth-service/src/app.module.ts
+++ auth-service/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TokenModule } from './token/token.module';
 
 @Module({
-  imports: [],
+  imports: [TokenModule],
   controllers: [],
   providers: [],
 })
