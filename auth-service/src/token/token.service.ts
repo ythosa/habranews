ea009171d9ebcf18b7dae72b6cd9b676077fcccc
ythosa <vasus714@yandex.ru>
2020-11-24 16:16:23 +0300

    Update token service

--- auth-service/src/token/token.service.ts
+++ auth-service/src/token/token.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
 import { CryptPasswordDto } from './dto/crypt-password.dto';
 import { GenerateTokensDto } from './dto/generate-token.dto';
 import { VerifyByAccessTokenDto } from './dto/verify-by-acess-token,dto';
@@ -6,10 +6,24 @@ import { CryptedPasswordImpl } from './interfaces/crypted-password.interface';
 import { TokensImpl } from './interfaces/tokens.interface';
 import { UserIdImpl } from './interfaces/user.interface';
 import * as bcrypt from 'bcrypt';
+import { ClientGrpc } from '@nestjs/microservices';
+import { UserServiceImpl } from './interfaces/user-service.interface';
 
 @Injectable()
 export class TokenService {
+    private userService: UserServiceImpl;
+
+    constructor(
+        @Inject(CACHE_MANAGER) private refreshTokensManager: Cache,
+        @Inject('USER_PACKAGE') private client: ClientGrpc
+    ) {}
+
+    onModuleInit() {
+        this.userService = this.client.getService<UserServiceImpl>('UserService');
+    }
+
     generateTokens(generateTokensDto: GenerateTokensDto): Promise<TokensImpl> {
+        // const user =
         throw new Error('Method not implemented.');
     }
 
