f5d34a8cb13717b060b6579cef4a014623c061fc
ythosa <vasus714@yandex.ru>
2020-11-25 12:34:59 +0300

    Add refresh token guard

---
+++ auth-service/src/token/guards/refresh-token.guard.ts
@@ -0,0 +1,34 @@
+import { CACHE_MANAGER, CanActivate, ExecutionContext, Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Cache } from 'cache-manager';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+
+export class RefreshTokenGuard implements CanActivate {
+    constructor(
+        @Inject(CACHE_MANAGER) private refreshTokensManager: Cache,
+        private readonly jwtService: JwtService,
+        private readonly configService: ConfigService,
+    ) {};
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const requestData = context.switchToRpc().getData();
+        const refreshToken = requestData.refreshToken;
+
+        let payload: JwtPayload;
+        try {
+            payload = this.jwtService.verify(refreshToken, { 
+                secret: this.configService.get<string>('REFRESH_JWT_SECRET'),
+            });
+        } catch (e) {
+            return false;
+        }
+
+        const storedToken = await this.refreshTokensManager.get(payload.id.toString());
+        if (storedToken != refreshToken) {
+            return false;
+        }
+
+        return true;
+    }
+}
