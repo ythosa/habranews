a11fe8e0153a36c15e0707fe9e5918dffada7d3e
ythosa <vasus714@yandex.ru>
2020-12-03 16:32:32 +0300

    Logging

--- api-gateway/src/tags/tags.controller.ts
+++ api-gateway/src/tags/tags.controller.ts
@@ -1,14 +1,18 @@
 import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { TagsImpl } from 'src/user/interfaces/tags.interface';
 import { TagsService } from './tags.service';
 
 @Controller('tags')
 export class TagsController {
+    private readonly logger = new Logger();
+    
     constructor(private readonly tagsService: TagsService) {}
 
     @Get('/getAvailableTags')
     getAvailableTags(): Promise<TagsImpl> {
+        this.logger.log('Getting all available tags...');
+        
         return this.tagsService.getAvailableTags();
     }
 }
--- parser-service/src/tags/tags.controller.ts
+++ parser-service/src/tags/tags.controller.ts
@@ -1,27 +1,31 @@
 import { RabbitRPC } from '@golevelup/nestjs-rabbitmq';
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import { TagsImpl } from './interfaces/tags.interface';
 import { TagsService } from './tags.service';
 
 @Controller('tags')
 export class TagsController {
+    private readonly logger = new Logger();
+    
     constructor(private readonly tagsService: TagsService) {}
 
     @RabbitRPC({
         exchange: 'tags-exchange',
         routingKey: 'rpc-route',
-        queue: 'get-tags-queue',
     })
     public async getTagsHandler(): Promise<TagsImpl> {
-        return this.tagsService.getAvailableTags();
+        this.logger.log('Getting all available tags...');
+
+        return await this.tagsService.getAvailableTags();
     }
 
     @RabbitRPC({
         exchange: 'tags-exchange',
         routingKey: 'subscribe-route',
-        queue: 'subscribe-queue',
     })
     public async patchTagsHandler(msg: TagsImpl) {
+        this.logger.log(`Patching tags with data: ${JSON.stringify(msg)}`);
+
         this.tagsService.patchTags(msg);
     }
 }
